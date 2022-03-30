a2a2dffd16d84ef2ac9a8100aa465ad9067cb9e2
Aaimry <wheal28@gmail.com>
2022-03-30 21:05:04 +0600

    Установили библиотеку Django Rest Framework Создали приложение api_v1 и зарегистрировали его в settings.py

--- source/hello/settings.py
+++ source/hello/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 
     'store',
     'accounts',
+    'api_v1'
 ]
 
 MIDDLEWARE = [
